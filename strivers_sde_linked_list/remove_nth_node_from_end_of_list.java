109195a7c0a90a65040fd84052931c8a39abaa99
Nandita <166365496+seriousprofile@users.noreply.github.com>
2024-12-25 19:57:23 +0530

    Create remove_nth_node_from_end_of_list.java

---
+++ strivers_sde_linked_list/remove_nth_node_from_end_of_list.java
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode dummy = new ListNode(0); // Dummy node to handle edge cases
+        dummy.next = head;
+        ListNode fast = dummy;
+        ListNode slow = dummy; 
+        
+        // Move the fast pointer n+1 steps ahead to maintain the gap
+        for (int i = 0; i <= n; i++) {
+            fast = fast.next;
+        }
+        
+        // Move both pointers until fast reaches the end
+        while (fast != null) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+        
+        // Remove the nth node from the end
+        slow.next = slow.next.next;
+        
+        return dummy.next; // Return the new head
+    }
+}
