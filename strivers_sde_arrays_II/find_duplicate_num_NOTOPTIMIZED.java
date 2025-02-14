07ad57b9519c8f70078b2cb71c42b18a901e1abf
nando <166365496+seriousprofile@users.noreply.github.com>
2025-02-14 15:38:01 +0530

    did this myself and got too happy for a sec
    ..then i realized this isnt optimized like COME ON

---
+++ strivers_sde_arrays_II/find_duplicate_num_NOTOPTIMIZED.java
@@ -0,0 +1,20 @@
+/* sorted array for easier comparison. 
+simply checked if prev element is the same as the current one! 
+however this isn't optimized because TC is O(NlogN) and there's a way to get O(N) approach! */
+
+class Solution {
+    public int findDuplicate(int[] nums) {
+        Arrays.sort(nums);
+        int repeatednum = nums[0];
+
+        for(int i = 1; i < nums.length; i++){
+            if(nums[i - 1] == nums[i]){
+                repeatednum = nums[i];
+            } else {
+                i++;
+            }
+        }
+
+        return repeatednum;
+    }
+}
