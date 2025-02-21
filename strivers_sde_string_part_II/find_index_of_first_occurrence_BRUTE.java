0a431e44e8a259b8953654fd349f2af3c57ecf90
nando <166365496+seriousprofile@users.noreply.github.com>
2025-02-21 19:37:57 +0530

    updated and dusted

--- strivers_sde_string_part_II/find_index_of_first_occurrence_BRUTE.java
+++ strivers_sde_string_part_II/find_index_of_first_occurrence_BRUTE.java
@@ -1,16 +1,18 @@
-/*
-NOTES:
-for the brute force approach, just compare every substring of haystack to needle. if they're equal, then return that index. 
-otherwise, just return -1!
+/* 
+notes:
+in this, you run a loop through both and check every substring of size n (needle.length()) to see
+if it matches the substring of size m (haystack.length()).
+if it does, return the starting index.
+otherwise, return -1.
 */
 
 class Solution {
     public int strStr(String haystack, String needle) {
-        int m = haystack.length(); 
+        int m = haystack.length();
         int n = needle.length();
 
         if(n > m){
-            return -1; 
+            return -1;
         }
 
         for(int i = 0; i <= m - n; i++){
@@ -19,6 +21,6 @@ class Solution {
             }
         }
 
-        return -1;
+        return -1; 
     }
 }
